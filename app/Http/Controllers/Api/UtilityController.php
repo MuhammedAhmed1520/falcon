af5ecb9baa432fbe9cee51ac82ae7ecb2da9593d
unknown <muhammedahmed1520@gmail.com>
2020-11-08 15:16:54 +0200

    recode

--- app/Http/Controllers/Api/UtilityController.php
+++ app/Http/Controllers/Api/UtilityController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Modules\Option\OptionRepo;
+use App\Soap\Request\GetConversionAmount;
+use App\Soap\Response\GetConversionAmountResponse;
+use Artisaninweb\SoapWrapper\SoapWrapper;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,15 +13,79 @@ class UtilityController extends Controller
 {
 
     private $optionRepo;
+    private $soapWrapper;
 
     public function __construct()
     {
         $this->optionRepo = new OptionRepo();
+        $this->soapWrapper = new SoapWrapper();
+
     }
     public function allOptions($type = null)
     {
         return $this->optionRepo->getOptionByType($type);
 
+    }
+
+    public function _testSoap()
+    {
+        $this->soapWrapper->add('Currency', function ($service) {
+            $service
+                ->wsdl('http://currencyconverter.kowabunga.net/converter.asmx?WSDL')
+                ->trace(true)
+                ->options(['user_agent' => 'PHPSoapClient'])
+                ->classmap([
+                    GetConversionAmount::class,
+                    GetConversionAmountResponse::class,
+                ]);
+        });
+        // Without classmap
+//        $response = $this->soapWrapper->call('Currency.GetConversionAmount', [
+//            'CurrencyFrom' => 'USD',
+//            'CurrencyTo'   => 'EUR',
+//            'RateDate'     => '2014-06-05',
+//            'Amount'       => '1000',
+//        ]);
+
+
+        // With classmap
+        $response = $this->soapWrapper->call('Currency.GetConversionAmount', [
+            new GetConversionAmount('USD', 'EG', '2020-11-08', '1000')
+        ]);
+
+        dd($response);
+        var_dump($response);
+        exit;
+    }
+    public function testSoap()
+    {
+        $this->soapWrapper->add('Falcon', function ($service) {
+            $service
+                ->wsdl('http://eprocesstest1.epa.org.kw:7002/FalconWebServices-FalconWebServices-context-root/FalconWebservicePort?WSDL')
+                ->trace(true)
+                ->options(['user_agent' => 'PHPSoapClient'])
+                ->classmap([
+                    GetConversionAmount::class,
+                    GetConversionAmountResponse::class,
+                ]);
+        });
+        // Without classmap
+//        $response = $this->soapWrapper->call('Currency.GetConversionAmount', [
+//            'CurrencyFrom' => 'USD',
+//            'CurrencyTo'   => 'EUR',
+//            'RateDate'     => '2014-06-05',
+//            'Amount'       => '1000',
+//        ]);
+
+
+        // With classmap
+        $response = $this->soapWrapper->call('Currency.GetConversionAmount', [
+            new GetConversionAmount('USD', 'EG', '2020-11-08', '1000')
+        ]);
+
+        dd($response);
+        var_dump($response);
+        exit;
     }
 
 }
---
+++ app/Soap/Request/SubmitFalconRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Soap\Request;
+
+
+class SubmitFalconRequest
+{
+    protected $P_REQUEST_TYP;
+
+    protected $P_O_CIVIL_ID;
+    protected $P_O_A_NAME;
+    protected $P_O_ADDRESS;
+    protected $P_O_MOBILE;
+    protected $P_O_PASSPORT_NO;
+    protected $P_CIVIL_EXPIRY_DT;
+    protected $P_O_MAIL;
+    protected $P_NW_CIVIL_ID;
+    protected $P_NW_A_NAME;
+    protected $P_NW_ADDRESS;
+    protected $P_NW_MOBILE;
+    protected $P_NW_PASSPORT_NO;
+    protected $P_NW_CIVIL_EXPIRY;
+    protected $P_NW_O_MAIL;
+    protected $P_CUR_PASS_FAL;
+    protected $P_FAL_SEX;
+    protected $P_FAL_SPECIES;
+    protected $P_FAL_TYPE;
+    protected $P_FAL_OTHER_TYPE;
+    protected $P_FAL_ORIGIN_COUNTRY;
+    protected $P_FAL_CITES_NO;
+    protected $P_FAL_PIT_NO;
+    protected $P_FAL_RING_NO;
+    protected $P_FAL_INJ_DATE;
+    protected $P_FAL_INJ_HOSPITAL;
+    protected $P_PAYMENT_ID;
+    protected $P_AMOUNT;
+//    protected $P_AMOUNT;
+
+
+
+
+}
