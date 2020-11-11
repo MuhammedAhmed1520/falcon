b3a3305316014e1d8755c3977aed223f7eb0b4d3
unknown <muhammedahmed1520@gmail.com>
2020-11-11 15:26:50 +0200

    recode

--- app/Modules/Payment/PaymentHelper.php
+++ app/Modules/Payment/PaymentHelper.php
@@ -10,10 +10,39 @@ namespace App\Modules\Payment;
 
 
 
+use App\Models\Falcon;
+use App\Modules\Falcon\FalconRepository;
 use Carbon\Carbon;
 
 trait PaymentHelper
 {
+
+    protected function updateMorph($pay,$knet)
+    {
+        $payment_type = $pay->paymentable_type;
+        switch ($payment_type){
+            case Falcon::class:
+                $falcon = $pay->paymentable;
+
+                $falconRepo = new FalconRepository();
+                $falconRepo->sendSoap([
+                   'P_REQUEST_TYP' => 6,
+                   'P_O_CIVIL_ID' => $falcon->P_O_CIVIL_ID,
+                   'P_PAYMENT_ID' => $knet['payment_id'] ?? null,
+                   'P_AMOUNT' => $knet['amount'] ?? null,
+                   'P_TRANS_ID' => $knet['tran_id'] ?? null,
+                   'P_TRACK_ID' => $knet['track_id'] ?? null,
+                ]);
+
+                break;
+            default:
+                break;
+        }
+
+
+
+
+    }
 
 
 }
